a56275ea666c665349b612f8ed0521a867570f43
yuksekyusuf <68251921+yuksekyusuf@users.noreply.github.com>
2022-04-29 18:37:57 +0200

    Initial Commit

---
+++ Livad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Livad
+//
+//  Created by Ahmet Yusuf Yuksek on 4/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Livad/LivadApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivadApp.swift
+//  Livad
+//
+//  Created by Ahmet Yusuf Yuksek on 4/29/22.
+//
+
+import SwiftUI
+
+@main
+struct LivadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
